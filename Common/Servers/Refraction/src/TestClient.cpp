3a03ead55b31adfe7afec5c3496698e626c909bc
Carlo Migoni <migoni@oa-cagliari.inaf.it>
2011-07-20 10:04:21 +0000

    test

--- Common/Servers/Refraction/src/TestClient.cpp
+++ Common/Servers/Refraction/src/TestClient.cpp
@@ -14,7 +14,11 @@ int main (int argc, char *argv[])
 	SimpleClient *client = new SimpleClient;
 	ACSErr::Completion_var completion;
 	Antenna::Refraction_var Refraction;
+	ofstream m_file;
+	IRA::CString out;
+        IRA::CString fileName = "stringtest";
 
+	m_file.open ((const char *) fileName, ios_base::out | ios_base::app);
 
   	/**
    	* Create the instance of Client and Init() it.
@@ -34,6 +38,7 @@ int main (int argc, char *argv[])
 
 
 	//Get reference to the component
+/*
 	ACS_SHORT_LOG((LM_INFO, "Getting component: %s","ANTENNA/Refraction"));
   	try
   	{
@@ -49,6 +54,7 @@ int main (int argc, char *argv[])
 		ACS_SHORT_LOG((LM_ERROR,"Error!"));
 		return -1;
    	}
+*/
 	ACS_SHORT_LOG((LM_INFO, "OK activation component: %s","Refraction"));
 
 	double zenith, zenithCorrected;
@@ -58,17 +64,24 @@ int main (int argc, char *argv[])
 	double i = 0.0;
 	for (i = -10.0; i < 9.0; i++) {
 		zenith = 90 - i*10.0;
+		zenithCorrected = 90 - i*5.0;
 		printf("Elevation = %f\n", i*10.0);
-		//Refraction->setMeteoParameters(20-i, 0.5+i/100., 1013+i);
-		Refraction->getCorrection(zenith*DD2R, zenithCorrected);
+//		Refraction->setMeteoParameters(20-i, 0.5+i/100., 1013+i);
+//		Refraction->getCorrection(zenith*DD2R, zenithCorrected);
 		printf ("zenithCorrected = %f arcseconds\n", zenithCorrected*DR2AS);
+		out.Format("zenithCorrected = %f arcseconds\n", zenithCorrected*DR2AS);
+		std::cout << (const char *) out << std::endl;
+		m_file << (const char *) out;
 		printf ("elevation = %f, elevation corrected = %f\n", i*10, i*10+zenithCorrected*DR2D);
+		out.Format("elevation = %f, elevation corrected = %f\n", i*10, i*10+zenithCorrected*DR2D);
+		m_file << (const char *) out;
+		std::cout << (const char *) out << std::endl;
 		ACE_OS::sleep (1);
 	}
 
 	ACE_OS::sleep (3);
 	ACS_SHORT_LOG ((LM_INFO, "Releasing..."));
-  	client->manager ()->release_component (client->handle (), "ANTENNA/Refraction");
+//  	client->manager ()->release_component (client->handle (), "ANTENNA/Refraction");
   	client->logout ();
   	delete client;
 
