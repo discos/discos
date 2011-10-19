8b2f50f9bd363d0971f9cd3cb214bf0c52f3bddb
Carlo Migoni <migoni@oa-cagliari.inaf.it>
2011-10-19 10:16:21 +0000

    revised some parameters

--- Common/Libraries/IRALibrary/src/SourceFlux.cpp
+++ Common/Libraries/IRALibrary/src/SourceFlux.cpp
@@ -27,15 +27,62 @@ double CSourceFlux::computeSourceFlux(const double&frequency, const double& FWHM
     double logfreq,logflux,fluxv;
     double fac0,fac1,fac2,fac4,fac5,fac;
     double corr, size;
+    double fMin=0.0, fMax=0.0;
+    double fMinRange[3], fMaxRange[3];
+    double fluxCoeff1[3], fluxCoeff2[3], fluxCoeff3[3];
+    double fcoeff0=0.0, fcoeff1=0.0, fcoeff2=0.0;
 
     fluxv=1.0;
     corr=1.0;
     size=0.0;
 
-    
-    if (m_flux.init && frequency >= m_flux.fmin && frequency <= m_flux.fmax) {
+    if (m_flux.nFreqRange == 1) {
+        sscanf(m_flux.freqRange,"%lf-%lf",&fMinRange[0],&fMaxRange[0]);
+        fMin = fMinRange[0]; fMax = fMaxRange[0];
+        sscanf(m_flux.fluxCoeff1,"%lf",&fluxCoeff1[0]); fcoeff0 = fluxCoeff1[0];
+        sscanf(m_flux.fluxCoeff2,"%lf",&fluxCoeff2[0]); fcoeff1 = fluxCoeff2[0];
+        sscanf(m_flux.fluxCoeff3,"%lf",&fluxCoeff3[0]); fcoeff2 = fluxCoeff3[0];
+    }
+    if (m_flux.nFreqRange == 2) {
+        sscanf(m_flux.freqRange,"%lf-%lf,%lf-%lf",&fMinRange[0],&fMaxRange[0],&fMinRange[1],&fMaxRange[1]);
+        if (frequency >= fMinRange[0] && frequency <= fMaxRange[0]) {
+            fMin = fMinRange[0]; fMax = fMaxRange[0];
+            sscanf(m_flux.fluxCoeff1,"%lf",&fluxCoeff1[0]); fcoeff0 = fluxCoeff1[0];
+            sscanf(m_flux.fluxCoeff2,"%lf",&fluxCoeff2[0]); fcoeff1 = fluxCoeff2[0];
+            sscanf(m_flux.fluxCoeff3,"%lf",&fluxCoeff3[0]); fcoeff2 = fluxCoeff3[0];
+        }
+        if (frequency >= fMinRange[1] && frequency <= fMaxRange[1]) {
+            fMin = fMinRange[1]; fMax = fMaxRange[1];
+            sscanf(m_flux.fluxCoeff1,"%lf",&fluxCoeff1[1]); fcoeff0 = fluxCoeff1[1];
+            sscanf(m_flux.fluxCoeff2,"%lf",&fluxCoeff2[1]); fcoeff1 = fluxCoeff2[1];
+            sscanf(m_flux.fluxCoeff3,"%lf",&fluxCoeff3[1]); fcoeff2 = fluxCoeff3[1];
+        }
+    }
+    if (m_flux.nFreqRange == 3) {
+        sscanf(m_flux.freqRange,"%lf-%lf,%lf-%lf,%lf-%lf",&fMinRange[0],&fMaxRange[0],&fMinRange[1],&fMaxRange[1],&fMinRange[2],&fMaxRange[2]);
+        if (frequency >= fMinRange[0] && frequency <= fMaxRange[0]) {
+            fMin = fMinRange[0]; fMax = fMaxRange[0];
+            sscanf(m_flux.fluxCoeff1,"%lf",&fluxCoeff1[0]); fcoeff0 = fluxCoeff1[0];
+            sscanf(m_flux.fluxCoeff2,"%lf",&fluxCoeff2[0]); fcoeff1 = fluxCoeff2[0];
+            sscanf(m_flux.fluxCoeff3,"%lf",&fluxCoeff3[0]); fcoeff2 = fluxCoeff3[0];
+        }
+        if (frequency >= fMinRange[1] && frequency <= fMaxRange[1]) {
+            fMin = fMinRange[1]; fMax = fMaxRange[1];
+            sscanf(m_flux.fluxCoeff1,"%lf",&fluxCoeff1[1]); fcoeff0 = fluxCoeff1[1];
+            sscanf(m_flux.fluxCoeff2,"%lf",&fluxCoeff2[1]); fcoeff1 = fluxCoeff2[1];
+            sscanf(m_flux.fluxCoeff3,"%lf",&fluxCoeff3[1]); fcoeff2 = fluxCoeff3[1];
+        }
+        if (frequency >= fMinRange[2] && frequency <= fMaxRange[2]) {
+            fMin = fMinRange[2]; fMax = fMaxRange[2];
+            sscanf(m_flux.fluxCoeff1,"%lf",&fluxCoeff1[2]); fcoeff0 = fluxCoeff1[2];
+            sscanf(m_flux.fluxCoeff2,"%lf",&fluxCoeff2[2]); fcoeff1 = fluxCoeff2[2];
+            sscanf(m_flux.fluxCoeff3,"%lf",&fluxCoeff3[2]); fcoeff2 = fluxCoeff3[2];
+        }
+    }
+
+    if (m_flux.init && frequency >= fMin && frequency <= fMax) {
         logfreq=log10(frequency);
-        logflux=m_flux.fcoeff[0]+logfreq*m_flux.fcoeff[1]+logfreq*logfreq*m_flux.fcoeff[2];
+        logflux=fcoeff0+logfreq*fcoeff1+logfreq*logfreq*fcoeff2;
         fluxv=pow(10.0,logflux);
         if(strcmp("casa",m_flux.name)==0) {
             double dflux;
@@ -45,7 +92,7 @@ double CSourceFlux::computeSourceFlux(const double&frequency, const double& FWHM
             IRA::CIRATools::getTime (now);
             tS.value (now.value().value);
             epoch = tS.year () + tS.dayOfYear () / 366.0;
-            if (m_flux.fcoeff[0] > 5.7){  /*entry is for discredited 1977 flux*/
+            if (fcoeff0 > 5.7){  /*entry is for discredited 1977 flux*/
                 dflux = 1 - ( (0.97 - 0.30*(logfreq-3)) /100.0);
                 fluxv*= pow(10.0,(epoch-1980.0)*log10(dflux));
             } else {                         /*entry for preliminary 2006 flux*/
@@ -54,6 +101,10 @@ double CSourceFlux::computeSourceFlux(const double&frequency, const double& FWHM
             }
         }
         if((strcmp("gauss",m_flux.model)==0)) {
+            if ((strcmp("virgoa",m_flux.model)==0) && (frequency >= fMinRange[1] && frequency <= fMaxRange[1])) {
+                m_flux.mcoeff[0]=0.973;
+                m_flux.mcoeff[3]=0.027;
+            }
             fac1=m_flux.mcoeff[1]/FWHM;
             fac2=m_flux.mcoeff[2]/FWHM;
             fac4=m_flux.mcoeff[4]/FWHM;
@@ -68,10 +119,11 @@ double CSourceFlux::computeSourceFlux(const double&frequency, const double& FWHM
             corr=exp(4.0*log(2.0)*fac0*fac0);
         } else
 	        corr=1.0;
-            size=m_flux.size;
+        
+        m_SourceFlux = fluxv / corr;
     }
-
-    m_SourceFlux = fluxv / corr;
+    else
+        m_SourceFlux = 0.0;
 
     return m_SourceFlux;
 }
