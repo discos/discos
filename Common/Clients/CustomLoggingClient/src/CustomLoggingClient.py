acd5b4462f06348ecf0a5e3e4a38791cf337d609
Marco Bartolini <bartolini@ira.inaf.it>
2012-06-14 08:48:11 +0000

    decoupled logging management functions and added docstrings

--- Common/Clients/CustomLoggingClient/src/CustomLoggingClient.py
+++ Common/Clients/CustomLoggingClient/src/CustomLoggingClient.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
+"""
+CustomLoggingClient implements a Tkinter interface that monitors in realtime the ACS custom
+logging events generated runtime.
+@TODO: implement window scrolling and window resize
+
+"""
 import acscommon
 import Management
 from Acspy.Nc.Consumer import Consumer
 import logging
 import bisect
+import functools
 import Tkinter as tk
 from bsqueue import BoundedSortedQueue
 
@@ -18,10 +25,31 @@ Management.CustomLoggingData.__ge__ = lambda self, other: self.timeStamp >= othe
 Management.CustomLoggingData.__repr__ = lambda self: self.msg
 
 # Maximum size for log messages display
+"""
+The number of logging events visualized in the window and kept in memory
+"""
 MAX_SIZE = 30
 
+"""
+Handle a log_event adding it to a BoundedSortedQueue in memory and to the 
+corresponding GUI representation (in Tkinter this sould be a Listbox).
+@param log_event: the logging event.
+@param memory_queue: a bounded sorted queue
+@type memory_queue: IRAPy.bsqueue.BoundedSortedQueue
+@param gui_queue: a Tkinter widget.
+@type gui_queue: Tkinter.Listbox
+"""
+def handle_log_event(log_event, memory_queue, gui_queue):
+    index, popped = memory_queue.push(log_event)
+    if popped:
+        gui_queue.delete(0)
+    gui_queue.insert(index, log_event.msg)
+
 class App:
+    """Tkinter GUI application
+    """
     def __init__(self, master):
+        """Constructor."""
         self.record_list = BoundedSortedQueue(max_size=MAX_SIZE)
         frame = tk.Frame(master) #frame will be accessible via master
         frame.pack()
@@ -29,19 +57,16 @@ class App:
         self.list_box.pack(expand=True, fill=tk.BOTH)
         self.clear_button = tk.Button(frame, text="clear", command=self.clear)
         self.clear_button.pack(side=tk.LEFT)
-   
-    def handler(self, event):
-        index, popped = self.record_list.push(event)
-        if popped:
-            self.list_box.delete(0)
-        self.list_box.insert(index, event.msg)
-        logging.info(str(event))
+        #define the log event handler
+        self.handler = functools.partial(handle_log_event, memory_queue=self.record_list, gui_queue=self.list_box)
    
     def clear(self):
+        """Empty the event list."""
         self.record_list.clear()
         self.list_box.delete(0, self.list_box.size())
 
 if __name__ == '__main__':
+    from IRAPy import logger
     try:
         root = tk.Tk()
         root.wm_title("ACS custom logging client")
@@ -51,6 +76,7 @@ if __name__ == '__main__':
         consumer.consumerReady()
     except Exception, ex:
         print "exception caught: ", ex #TODO: throw excep
+        logger.logError(ex.message)
     try:
         root.mainloop()    
     except KeyboardInterrupt, ki:
