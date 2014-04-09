1e7e555c2707af8c85f8f206f2ebad71251b0df0
Marco Bartolini <bartolini@ira.inaf.it>
2014-04-09 14:17:51 +0000

    tries to fix behaviour for malformed xml log events

--- Common/Servers/CustomLogger/src/expat_log_parsing.cpp
+++ Common/Servers/CustomLogger/src/expat_log_parsing.cpp
@@ -243,9 +243,11 @@ get_log_record(XML_Parser log_parser, const char *xml_text)
         std::stringstream msg;
         msg << "CustomLoggerMalformedXMLError: ";
         msg << XML_ErrorString(XML_GetErrorCode(log_parser));
-        log_record->xml_text.assign(msg.str().c_str());
+        //log_record->xml_text.assign(msg.str().c_str());
         //log_record->xml_text.assign("CustomLoggerMalformedXMLError");
         ACE_ERROR ((LM_ERROR, msg.str().c_str() ));
+        //if errors discrads the malformed XML log record and creates a new one
+        log_record = get_log_record(msg.str().c_str(), Management::C_ERROR);
     }else{
         // XML is good to go
         log_record->xml_text.assign(xml_text);
