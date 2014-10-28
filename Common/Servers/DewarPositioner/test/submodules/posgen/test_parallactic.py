d9992e020b7bc0b842d046eed02c5f6927ff6b8b
Marco Buttu <mbuttu@oa-cagliari.inaf.it>
2014-10-28 11:20:41 +0000

    Added a test for the parallactic position generator

--- Common/Servers/DewarPositioner/test/submodules/posgen/test_parallactic.py
+++ Common/Servers/DewarPositioner/test/submodules/posgen/test_parallactic.py
@@ -4,12 +4,12 @@ from math import sin, cos, tan, atan, radians, degrees, pi
 from DewarPositioner.posgenerator import PosGenerator, PosGeneratorError
 
 
-class PosGeneratorFixedTest(unittest2.TestCase):
+class PosGeneratorParallacticTest(unittest2.TestCase):
 
     def setUp(self):
         self.m = mocker.Mocker()
         self.source = self.m.mock()
-        self.posgen = PosGenerator(timeout=2)
+        self.posgen = PosGenerator(zdtimeout=2)
 
     def tearDown(self):
         self.m.restore()
@@ -17,9 +17,9 @@ class PosGeneratorFixedTest(unittest2.TestCase):
 
     def test_elevation(self):
         """Raise a PosGeneratorError in case of wrong site_info"""
-        gen = self.posgen.fixed(self.source, site_info={'LATITUDE': 39})
+        gen = self.posgen.parallactic(self.source, siteInfo={'LATITUDE': 39})
         self.assertRaises(PosGeneratorError, gen.next)
-        gen = self.posgen.fixed(self.source, site_info=11)
+        gen = self.posgen.parallactic(self.source, siteInfo=11)
         self.assertRaises(PosGeneratorError, gen.next)
 
     def test_property_objs(self):
@@ -28,7 +28,7 @@ class PosGeneratorFixedTest(unittest2.TestCase):
         self.m.throw(RuntimeError)
         mocker.expect(self.source._get_name()).result('mocker')
         self.m.replay()
-        gen = self.posgen.fixed(self.source, site_info={'latitude': 39})
+        gen = self.posgen.parallactic(self.source, siteInfo={'latitude': 39})
         self.assertRaises(PosGeneratorError, gen.next)
 
     def test_property_values(self):
@@ -42,7 +42,7 @@ class PosGeneratorFixedTest(unittest2.TestCase):
         mocker.expect(self.source._get_elevation()).result(el_obj)
         mocker.expect(self.source._get_name()).result('mocker')
         self.m.replay()
-        gen = self.posgen.fixed(self.source, site_info={'latitude': 39})
+        gen = self.posgen.parallactic(self.source, siteInfo={'latitude': 39})
         self.assertRaises(PosGeneratorError, gen.next)
 
     def test_completion_error(self):
@@ -58,7 +58,7 @@ class PosGeneratorFixedTest(unittest2.TestCase):
         mocker.expect(self.source._get_elevation()).result(el_obj)
         mocker.expect(self.source._get_name()).result('mocker')
         self.m.replay()
-        gen = self.posgen.fixed(self.source, site_info={'latitude': 39})
+        gen = self.posgen.parallactic(self.source, siteInfo={'latitude': 39})
         self.assertRaises(PosGeneratorError, gen.next)
 
     def test_zero_division_timeout(self):
@@ -80,7 +80,7 @@ class PosGeneratorFixedTest(unittest2.TestCase):
         mocker.expect(self.source._get_elevation()).result(el_obj)
         self.m.count(1, None)
         self.m.replay()
-        gen = self.posgen.fixed(self.source, site_info={'latitude': 0})
+        gen = self.posgen.parallactic(self.source, siteInfo={'latitude': 0})
         self.assertRaises(PosGeneratorError, gen.next)
 
     def test_right_behavior(self):
@@ -102,7 +102,7 @@ class PosGeneratorFixedTest(unittest2.TestCase):
         mocker.expect(self.source._get_azimuth()).result(az_obj)
         mocker.expect(self.source._get_elevation()).result(el_obj)
         self.m.replay()
-        gen = self.posgen.fixed(self.source, site_info={'latitude': latitude})
+        gen = self.posgen.parallactic(self.source, siteInfo={'latitude': latitude})
         latitude = radians(latitude)
         for azd, eld in zip(azimuths, elevations):
             el = radians(eld)
