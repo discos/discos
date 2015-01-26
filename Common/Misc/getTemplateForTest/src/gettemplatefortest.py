008e8661d165fa8038b92603ac63482e837cc84a
Marco Bartolini <bartolini@ira.inaf.it>
2015-01-26 11:20:26 +0000

    added first structure for test templates

---
+++ Common/Misc/getTemplateForTest/src/gettemplatefortest.py
@@ -0,0 +1,44 @@
+import sys
+import os
+import shutil
+
+usage = """
+getTmplateForTest - create a test directory containing the necessary code templates
+                    used for component automated testing.
+                    This command should be run from within a Component directory at
+                    the first level of depth, creating a 'test' directory at the same level 
+                    of 'src, objects' ecc...
+"""                    
+
+def get_introot():
+    introot = os.getenv("INTROOT")
+    if not introot:
+        msg = "ERROR: INTROOT environment variable is not defined"
+        raise Exception(msg)
+    return introot
+
+def get_template_dir_name():
+    introot = get_introot()
+    return os.path.join(introot, "templates/test")
+
+def get_template_dir():
+    template_dir = get_template_dir_name()
+    if not os.path.exists(template_dir):
+        msg = "ERROR: template directory does not exist"
+        raise Exception(msg)
+    return template_dir
+
+def command_line_util(args):
+    try:
+        help = args[0] == "--help" or args[0] == "-h"
+    except:
+        help = False
+    if help:
+        print usage
+        sys.exit(0)
+    if os.path.exists("test"):
+        msg = "ERROR: test directory already exists."
+        raise Exception(msg)
+    template_dir = get_template_dir()
+    shutil.copytree(template_dir, "test")
+
