ba83cf602bf4026d8b1ea6fe96ac711080fa9603
Sergio Poppi <spoppi@oa-cagliari.inaf.it>
2015-02-04 12:28:14 +0000

    patched weater station

--- Noto/Servers/NotoWeatherStation/include/MeteoSocket.h
+++ Noto/Servers/NotoWeatherStation/include/MeteoSocket.h
@@ -9,7 +9,7 @@
 using namespace IRA;
 
 
-const static char WEATHERCMD[10] ="wx\n";
+const static char WEATHERCMD[10] ="LOOP\n";
 
 
 class MeteoSocket : public CSocket{
@@ -51,7 +51,9 @@ static int Depth;
 	double m_temperature;
 	double m_pressure;
 	double m_humidity;
-	int parse(const char* buff);
+        int parse(const char* buff);   // weather data from station
+        int fs_parse(const char* buff); // weather data from the field system
+
 //	MeteoData *m_sensorData;
 	 int updateParam();
 
