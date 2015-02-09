2c1feaafab1941e59a0dd877165ce4260349d07e
Marco Bartolini <bartolini@ira.inaf.it>
2015-02-09 16:29:34 +0000

    added jenkins scripts

---
+++ SystemBuild/build_srt_trunk_telescope.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This job runs a nightly build, remember to set jenkins to:
+# 1) set the workspace to point to repos/ACS/trunk on the svn server
+# 2) Poll svn for changes at night
+# 3) trigger mantis changes and file a ticket if project does not build
+
+source ~/.bashrc
+export TARGETSYS=SRT
+export INTROOT=/system/introot/EXECUTOR_$EXECUTOR_NUMBER
+export ACS_CDB=$WORKSPACE/SRT
+rm -rf $INTROOT
+getTemplateForDirectory INTROOT $INTROOT
+cd SystemMake
+make common-clean common-build
+make telescope-clean telescope-build
