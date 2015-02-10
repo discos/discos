965477b9ca3c16f92f697cdce3a418407169ba39
Marco Bartolini <bartolini@ira.inaf.it>
2015-02-10 13:02:56 +0000

    fixed build scripts declarations order

--- SystemBuild/build_med_trunk_common.sh
+++ SystemBuild/build_med_trunk_common.sh
@@ -4,10 +4,10 @@
 # 2) Poll svn for changes at night
 # 3) trigger mantis changes and file a ticket if project does not build
 
-source ~/.bashrc
 export TARGETSYS=MED
 export INTROOT=/system/introot/EXECUTOR_$EXECUTOR_NUMBER
 export ACS_CDB=$WORKSPACE/Medicina
+source ~/.bashrc
 rm -rf $INTROOT
 mkdir -p $INTROOT
 getTemplateForDirectory INTROOT $INTROOT
--- SystemBuild/build_nt_trunk_common.sh
+++ SystemBuild/build_nt_trunk_common.sh
@@ -4,11 +4,12 @@
 # 2) Poll svn for changes at night
 # 3) trigger mantis changes and file a ticket if project does not build
 
-source ~/.bashrc
 export TARGETSYS=NT
 export INTROOT=/system/introot/EXECUTOR_$EXECUTOR_NUMBER
 export ACS_CDB=$WORKSPACE/Noto
 rm -rf $INTROOT
+mkdir -p $INTROOT
+source ~/.bashrc
 getTemplateForDirectory INTROOT $INTROOT
 cd SystemMake
 make common-clean common-build
