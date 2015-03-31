02e67d901c428063def177b388d1f7ab24f0aded
Marco Bartolini <bartolini@ira.inaf.it>
2015-03-31 10:11:52 +0000

    fixed bug in package script

--- SystemBuild/package_stable_release.sh
+++ SystemBuild/package_stable_release.sh
@@ -9,16 +9,16 @@ chown -R manager:acs ${BASEDIR}
 cp -r ${INTROOT_PREFIX}/${TARGETSYS}/${REPO_VERSION}/STABLE/* ${BASEDIR}/introot/
 cp -r ${CDB_PREFIX}/${TARGETSYS}/${REPO_VERSION}/STABLE/* ${BASEDIR}/cdb/
 touch ${BASEDIR}/buildinfo.txt
-cat "build id:\t${BUILD_ID}\n" >> ${BASEDIR}/buildinfo.txt
-cat "build number:\t${BUILD_DISPLAY_NAME}\n" >> ${BASEDIR}/buildinfo.txt
-cat "build tag:\t${BUILD_TAG}\n" >> ${BASEDIR}/buildinfo.txt
-cat "svn revision:\t${SVN_REVISION}\n" >> ${BASEDIR}/buildinfo.txt
-cat "svn url:\t${SVN_URL}\n" >> ${BASEDIR}/buildinfo.txt
-cat "******************************************************\n"
-cat "* Azdora build parameters                            *\n"
-cat "******************************************************\n"
-cat "INTROOT_PREFIX:\t${INTROOT_PREFIX}\n" >> ${BASEDIR}/buildinfo.txt
-cat "CDB_PREFIX:\t${CDB_PREFIX}\n" >> ${BASEDIR}/buildinfo.txt
-cat "TARGETSYS:\t${TARGETSYS}\n" >> ${BASEDIR}/buildinfo.txt
-cat "REPO_VERSION:\t${REPO_VERSION}\n" >> ${BASEDIR}/buildinfo.txt
+echo "build id:\t${BUILD_ID}\n" >> ${BASEDIR}/buildinfo.txt
+echo "build number:\t${BUILD_DISPLAY_NAME}\n" >> ${BASEDIR}/buildinfo.txt
+echo "build tag:\t${BUILD_TAG}\n" >> ${BASEDIR}/buildinfo.txt
+echo "svn revision:\t${SVN_REVISION}\n" >> ${BASEDIR}/buildinfo.txt
+echo "svn url:\t${SVN_URL}\n" >> ${BASEDIR}/buildinfo.txt
+echo "******************************************************\n"
+echo "* Azdora build parameters                            *\n"
+echo "******************************************************\n"
+echo "INTROOT_PREFIX:\t${INTROOT_PREFIX}\n" >> ${BASEDIR}/buildinfo.txt
+echo "CDB_PREFIX:\t${CDB_PREFIX}\n" >> ${BASEDIR}/buildinfo.txt
+echo "TARGETSYS:\t${TARGETSYS}\n" >> ${BASEDIR}/buildinfo.txt
+echo "REPO_VERSION:\t${REPO_VERSION}\n" >> ${BASEDIR}/buildinfo.txt
 tar czvf discos-${TARGETSYS}-${BASENAME}-b${BUILD_NUMBER}.tar.gz introot cdb buildinfo.txt
