b8b1914074a78353bc5428ee4d3c4469f0742725
Marco Bartolini <bartolini@ira.inaf.it>
2015-03-31 10:24:34 +0000

    added bashrc to package

--- SystemBuild/package_stable_release.sh
+++ SystemBuild/package_stable_release.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -ex
-BASEDIR=discos/${TARGETSYS}/${REPO_VERSION}
 BASENAME=`basename ${REPO_VERSION}`
+BASEDIR=discos/${TARGETSYS}/${BASENAME}
+BUILDINFO=${BASEDIR}/buildinfo.txt
+BASHRC=${BASEDIR}/bashrc
 source ~/.bashrc
 rm -rf discos
 mkdir -p ${BASEDIR}/introot
@@ -8,17 +10,22 @@ mkdir -p ${BASEDIR}/cdb
 chown -R manager:acs ${BASEDIR}
 cp -r ${INTROOT_PREFIX}/${TARGETSYS}/${REPO_VERSION}/STABLE/* ${BASEDIR}/introot/
 cp -r ${CDB_PREFIX}/${TARGETSYS}/${REPO_VERSION}/STABLE/* ${BASEDIR}/cdb/
-touch buildinfo.txt
-echo "build id:\t${BUILD_ID}\n" >> buildinfo.txt
-echo "build number:\t${BUILD_DISPLAY_NAME}\n" >> buildinfo.txt
-echo "build tag:\t${BUILD_TAG}\n" >> buildinfo.txt
-echo "svn revision:\t${SVN_REVISION}\n" >> buildinfo.txt
-echo "svn url:\t${SVN_URL}\n" >> buildinfo.txt
-echo "******************************************************\n" >> buildinfo.txt
-echo "* Azdora build parameters                            *\n" >> buildinfo.txt
-echo "******************************************************\n" >> buildinfo.txt
-echo "INTROOT_PREFIX:\t${INTROOT_PREFIX}\n" >> buildinfo.txt
-echo "CDB_PREFIX:\t${CDB_PREFIX}\n" >> buildinfo.txt
-echo "TARGETSYS:\t${TARGETSYS}\n" >> buildinfo.txt
-echo "REPO_VERSION:\t${REPO_VERSION}\n" >> buildinfo.txt
-tar czvf discos-${TARGETSYS}-${BASENAME}-b${BUILD_NUMBER}.tar.gz discos buildinfo.txt
+touch ${BUILDINFO}
+echo "build id:\t${BUILD_ID}\n" >> ${BUILDINFO}
+echo "build number:\t${BUILD_DISPLAY_NAME}\n" >> ${BUILDINFO}
+echo "build tag:\t${BUILD_TAG}\n" >> ${BUILDINFO}
+echo "svn revision:\t${SVN_REVISION}\n" >> ${BUILDINFO}
+echo "svn url:\t${SVN_URL}\n" >> ${BUILDINFO}
+echo "******************************************************\n" >> ${BUILDINFO}
+echo "* Azdora build parameters                            *\n" >> ${BUILDINFO}
+echo "******************************************************\n" >> ${BUILDINFO}
+echo "INTROOT_PREFIX:\t${INTROOT_PREFIX}\n" >> ${BUILDINFO}
+echo "CDB_PREFIX:\t${CDB_PREFIX}\n" >> ${BUILDINFO}
+echo "TARGETSYS:\t${TARGETSYS}\n" >> ${BUILDINFO}
+echo "REPO_VERSION:\t${REPO_VERSION}\n" >> ${BUILDINFO}
+touch ${BASHRC}
+echo "export INTROOT=/${BASEDIR}/introot" >> ${BASHRC}
+echo "export ACS_CDB=/${BASEDIR}/cdb" >> ${BASHRC}
+echo "export TARGETSYS=${TARGETSYS}" >> ${BASHRC}
+echo "export REPO_VERSION=${REPO_VERSION}" >> ${BASHRC}
+tar czvf discos-${TARGETSYS}-${BASENAME}-b${BUILD_NUMBER}.tar.gz discos
