b9a5dd07572dede9534c9e54c44854d30b0c8747
Marco Bartolini <bartolini@ira.inaf.it>
2015-03-31 10:27:45 +0000

    replaced echo with printf in packaging scripts

--- SystemBuild/package_stable_release.sh
+++ SystemBuild/package_stable_release.sh
@@ -11,21 +11,21 @@ chown -R manager:acs ${BASEDIR}
 cp -r ${INTROOT_PREFIX}/${TARGETSYS}/${REPO_VERSION}/STABLE/* ${BASEDIR}/introot/
 cp -r ${CDB_PREFIX}/${TARGETSYS}/${REPO_VERSION}/STABLE/* ${BASEDIR}/cdb/
 touch ${BUILDINFO}
-echo "build id:\t${BUILD_ID}\n" >> ${BUILDINFO}
-echo "build number:\t${BUILD_DISPLAY_NAME}\n" >> ${BUILDINFO}
-echo "build tag:\t${BUILD_TAG}\n" >> ${BUILDINFO}
-echo "svn revision:\t${SVN_REVISION}\n" >> ${BUILDINFO}
-echo "svn url:\t${SVN_URL}\n" >> ${BUILDINFO}
-echo "******************************************************\n" >> ${BUILDINFO}
-echo "* Azdora build parameters                            *\n" >> ${BUILDINFO}
-echo "******************************************************\n" >> ${BUILDINFO}
-echo "INTROOT_PREFIX:\t${INTROOT_PREFIX}\n" >> ${BUILDINFO}
-echo "CDB_PREFIX:\t${CDB_PREFIX}\n" >> ${BUILDINFO}
-echo "TARGETSYS:\t${TARGETSYS}\n" >> ${BUILDINFO}
-echo "REPO_VERSION:\t${REPO_VERSION}\n" >> ${BUILDINFO}
+printf "build id:\t${BUILD_ID}\n" >> ${BUILDINFO}
+printf "build number:\t${BUILD_DISPLAY_NAME}\n" >> ${BUILDINFO}
+printf "build tag:\t${BUILD_TAG}\n" >> ${BUILDINFO}
+printf "svn revision:\t${SVN_REVISION}\n" >> ${BUILDINFO}
+printf "svn url:\t${SVN_URL}\n" >> ${BUILDINFO}
+printf "******************************************************\n" >> ${BUILDINFO}
+printf "* Azdora build parameters                            *\n" >> ${BUILDINFO}
+printf "******************************************************\n" >> ${BUILDINFO}
+printf "INTROOT_PREFIX:\t${INTROOT_PREFIX}\n" >> ${BUILDINFO}
+printf "CDB_PREFIX:\t${CDB_PREFIX}\n" >> ${BUILDINFO}
+printf "TARGETSYS:\t${TARGETSYS}\n" >> ${BUILDINFO}
+printf "REPO_VERSION:\t${REPO_VERSION}\n" >> ${BUILDINFO}
 touch ${BASHRC}
-echo "export INTROOT=/${BASEDIR}/introot" >> ${BASHRC}
-echo "export ACS_CDB=/${BASEDIR}/cdb" >> ${BASHRC}
-echo "export TARGETSYS=${TARGETSYS}" >> ${BASHRC}
-echo "export REPO_VERSION=${REPO_VERSION}" >> ${BASHRC}
+printf "export INTROOT=/${BASEDIR}/introot" >> ${BASHRC}
+printf "export ACS_CDB=/${BASEDIR}/cdb" >> ${BASHRC}
+printf "export TARGETSYS=${TARGETSYS}" >> ${BASHRC}
+printf "export REPO_VERSION=${REPO_VERSION}" >> ${BASHRC}
 tar czvf discos-${TARGETSYS}-${BASENAME}-b${BUILD_NUMBER}.tar.gz discos
