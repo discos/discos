c895c9d6031dd6c066a30749bcfb4750c36dbdbd
Marco Bartolini <bartolini@ira.inaf.it>
2015-03-31 10:15:22 +0000

    fixed bug in package script

--- SystemBuild/package_stable_release.sh
+++ SystemBuild/package_stable_release.sh
@@ -8,17 +8,17 @@ mkdir -p ${BASEDIR}/cdb
 chown -R manager:acs ${BASEDIR}
 cp -r ${INTROOT_PREFIX}/${TARGETSYS}/${REPO_VERSION}/STABLE/* ${BASEDIR}/introot/
 cp -r ${CDB_PREFIX}/${TARGETSYS}/${REPO_VERSION}/STABLE/* ${BASEDIR}/cdb/
-touch ${BASEDIR}/buildinfo.txt
-echo "build id:\t${BUILD_ID}\n" >> ${BASEDIR}/buildinfo.txt
-echo "build number:\t${BUILD_DISPLAY_NAME}\n" >> ${BASEDIR}/buildinfo.txt
-echo "build tag:\t${BUILD_TAG}\n" >> ${BASEDIR}/buildinfo.txt
-echo "svn revision:\t${SVN_REVISION}\n" >> ${BASEDIR}/buildinfo.txt
-echo "svn url:\t${SVN_URL}\n" >> ${BASEDIR}/buildinfo.txt
-echo "******************************************************\n"
-echo "* Azdora build parameters                            *\n"
-echo "******************************************************\n"
-echo "INTROOT_PREFIX:\t${INTROOT_PREFIX}\n" >> ${BASEDIR}/buildinfo.txt
-echo "CDB_PREFIX:\t${CDB_PREFIX}\n" >> ${BASEDIR}/buildinfo.txt
-echo "TARGETSYS:\t${TARGETSYS}\n" >> ${BASEDIR}/buildinfo.txt
-echo "REPO_VERSION:\t${REPO_VERSION}\n" >> ${BASEDIR}/buildinfo.txt
-tar czvf discos-${TARGETSYS}-${BASENAME}-b${BUILD_NUMBER}.tar.gz introot cdb buildinfo.txt
+touch buildinfo.txt
+echo "build id:\t${BUILD_ID}\n" >> buildinfo.txt
+echo "build number:\t${BUILD_DISPLAY_NAME}\n" >> buildinfo.txt
+echo "build tag:\t${BUILD_TAG}\n" >> buildinfo.txt
+echo "svn revision:\t${SVN_REVISION}\n" >> buildinfo.txt
+echo "svn url:\t${SVN_URL}\n" >> buildinfo.txt
+echo "******************************************************\n" >> buildinfo.txt
+echo "* Azdora build parameters                            *\n" >> buildinfo.txt
+echo "******************************************************\n" >> buildinfo.txt
+echo "INTROOT_PREFIX:\t${INTROOT_PREFIX}\n" >> buildinfo.txt
+echo "CDB_PREFIX:\t${CDB_PREFIX}\n" >> buildinfo.txt
+echo "TARGETSYS:\t${TARGETSYS}\n" >> buildinfo.txt
+echo "REPO_VERSION:\t${REPO_VERSION}\n" >> buildinfo.txt
+tar czvf discos-${TARGETSYS}-${BASENAME}-b${BUILD_NUMBER}.tar.gz discos buildinfo.txt
