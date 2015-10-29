c6e5468e2d5aa03fd0066aae78198d096b6f915e
Marco Buttu <mbuttu@oa-cagliari.inaf.it>
2015-10-29 10:53:45 +0000

    When a container is not available, catch the exception and look for the others

--- SRT/Misc/SRTScripts/src/receiversmonitor.py
+++ SRT/Misc/SRTScripts/src/receiversmonitor.py
@@ -7,7 +7,7 @@ from Acspy.Clients.SimpleClient import PySimpleClient
 from Acspy.Util.ACSCorba import getManager
 
 logging.basicConfig(
-    filename='/home/gavino/logs/receivers.log', 
+    filename='/home/gavino/logs/receivers.log',
     format='%(asctime)s\t%(message)s')
 
 receivers = ['SRTLPBandReceiver', 'SRTKBandMFReceiver', 'SRT7GHzReceiver']
@@ -19,21 +19,28 @@ while True:
             components = []
             client = PySimpleClient()
             for receiver in receivers:
-                component = client.getComponent('RECEIVERS/' + receiver)
-                components.append(component)
+                try:
+                    component = client.getComponent('RECEIVERS/' + receiver)
+                    components.append(component)
+                except:
+                    pass
 
-        except Exception:
-            pass
-        except KeyboardInterrupt:
-            logging.info('program closed by the user')
-            raise
-        try:
             for component in components:
                 temp_obj = component._get_cryoTemperatureLNA()
                 value, comp = temp_obj.get_sync()
                 name = component._get_name().split('/')[-1]
                 logging.info('  %s%.2f' % (name.ljust(22), value))
+        except KeyboardInterrupt:
+            logging.info('program closed by the user')
+            raise
         except:
             pass
+        finally:
+            try:
+                if getManager():
+                    client.disconnect()
+            except:
+                logging.error('can not disconnect the client')
 
-    time.sleep(60)
+    time.sleep(180)
+
