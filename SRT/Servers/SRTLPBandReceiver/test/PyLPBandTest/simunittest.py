95bcdbacdc5bd8b5b8bc07f007943922be1c166f
Marco Buttu <mbuttu@oa-cagliari.inaf.it>
2015-11-13 16:13:48 +0000

    Added derotator simulator to SRT/Simulators/Components

---
+++ SRT/Servers/SRTLPBandReceiver/test/PyLPBandTest/simunittest.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import time
+import unittest2
+import subprocess
+
+def run(test_case):
+    # Use the simulators and the testing CDB
+    server_name = 'receiverboard-sim' if test_case.telescope == 'SRT' else ''
+    if not server_name:
+        sys.exit(0) 
+
+    FNULL = open(os.devnull, 'w')
+    try:
+        subprocess.Popen(['%s start' %server_name], stdout=FNULL, stderr=FNULL)
+        time.sleep(1) # Give the server the time to start
+        suite = unittest2.TestSuite()
+        tests = unittest2.TestLoader().loadTestsFromTestCase(test_case)
+        suite.addTests(tests)
+        print 'Running the tests using the antenna simulators...'
+        unittest2.TextTestRunner(verbosity=2).run(suite)
+    finally:
+        subprocess.Popen(['%s stop' %server_name], stdout=FNULL, stderr=FNULL)
+        time.sleep(2) # Give the server the time to stop
