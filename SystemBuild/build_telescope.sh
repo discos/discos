46ee4870cd9359149d6bf8e7442dd18514bc80ef
Marco Bartolini <bartolini@ira.inaf.it>
2016-01-21 14:27:36 +0000

    added production option to build scripts

--- SystemBuild/build_telescope.sh
+++ SystemBuild/build_telescope.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+PRODUCTION=""
+while getopts "p" opt; do
+    case $opt in
+      p)
+        echo "compiling in production mode"
+        PRODUCTION="DEBUG=''"
+        ;;
+    esac
+done
+
 source ~/.bashrc
 if [ ! -d "$INTROOT" ]; then
     echo "cannot find INTROOT in $INTROOT"
     exit 1
 fi
 cd SystemMake
-make telescope-clean telescope-build
+make $PRODUCTION telescope-clean telescope-build
