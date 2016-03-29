b43496e57e8b40341198aea8bdf020f6ac7bb27e
Marco Bartolini <bartolini@ira.inaf.it>
2016-03-29 12:11:23 +0000

    fixed bug in telescope build script

--- SystemBuild/build_telescope.sh
+++ SystemBuild/build_telescope.sh
@@ -11,7 +11,7 @@ while getopts "p" opt; do
 done
 
 source ~/.bashrc
-if [ -d "$DISCOS_CDB"]; then
+if [ -d "$DISCOS_CDB" ]; then
     echo "replacing $ACS_CDB with $DISCOS_CDB"
     ACS_CDB=$DISCOS_CDB
 fi
