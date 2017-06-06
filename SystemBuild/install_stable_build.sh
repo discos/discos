6ea3c1b8945ad81733d6cd2daa84a276ca32efd8
Marco Buttu <marco.buttu@gmail.com>
2017-06-06 10:13:26 +0000

    Fix #118: rename TARGETSYS to STATION

--- SystemBuild/install_stable_build.sh
+++ SystemBuild/install_stable_build.sh
@@ -4,8 +4,8 @@ if [ -d "$DISCOS_CDB" ]; then
     echo "replacing $ACS_CDB with $DISCOS_CDB"
     ACS_CDB=$DISCOS_CDB
 fi
-NEW_INTROOT=${INTROOT_PREFIX}/${TARGETSYS}/${REPO_VERSION}/STABLE
-NEW_CDB=${CDB_PREFIX}/${TARGETSYS}/${REPO_VERSION}/STABLE
+NEW_INTROOT=${INTROOT_PREFIX}/${STATION}/${REPO_VERSION}/STABLE
+NEW_CDB=${CDB_PREFIX}/${STATION}/${REPO_VERSION}/STABLE
 rm -rf $NEW_INTROOT
 rm -rf $NEW_CDB
 mkdir -p $NEW_INTROOT
@@ -14,4 +14,4 @@ cp -r $INTROOT/* $NEW_INTROOT/
 mkdir -p $NEW_CDB
 chmod -R u+rwx $NEW_CDB
 cp -r ${ACS_CDB}/CDB ${NEW_CDB}/
-echo "Installed new stable version of $TARGETSYS"
+echo "Installed new stable version of $STATION"
--- SystemBuild/package_stable_release.sh
+++ SystemBuild/package_stable_release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 BASENAME=`basename ${REPO_VERSION}`
-BASEDIR=discos/${TARGETSYS}/${BASENAME}
+BASEDIR=discos/${STATION}/${BASENAME}
 BUILDINFO=${BASEDIR}/buildinfo.txt
 BASHRC=${BASEDIR}/bashrc
 source ~/.bashrc
@@ -12,8 +12,8 @@ rm -rf discos
 mkdir -p ${BASEDIR}/introot
 mkdir -p ${BASEDIR}/cdb
 chown -R manager:acs ${BASEDIR}
-cp -r ${INTROOT_PREFIX}/${TARGETSYS}/${REPO_VERSION}/STABLE/* ${BASEDIR}/introot/
-cp -r ${CDB_PREFIX}/${TARGETSYS}/${REPO_VERSION}/STABLE/* ${BASEDIR}/cdb/
+cp -r ${INTROOT_PREFIX}/${STATION}/${REPO_VERSION}/STABLE/* ${BASEDIR}/introot/
+cp -r ${CDB_PREFIX}/${STATION}/${REPO_VERSION}/STABLE/* ${BASEDIR}/cdb/
 touch ${BUILDINFO}
 printf "build id:\t${BUILD_ID}\n" >> ${BUILDINFO}
 printf "build number:\t${BUILD_DISPLAY_NAME}\n" >> ${BUILDINFO}
@@ -25,11 +25,11 @@ printf "* Azdora build parameters                            *\n" >> ${BUILDINFO
 printf "******************************************************\n" >> ${BUILDINFO}
 printf "INTROOT_PREFIX:\t${INTROOT_PREFIX}\n" >> ${BUILDINFO}
 printf "CDB_PREFIX:\t${CDB_PREFIX}\n" >> ${BUILDINFO}
-printf "TARGETSYS:\t${TARGETSYS}\n" >> ${BUILDINFO}
+printf "STATION:\t${STATION}\n" >> ${BUILDINFO}
 printf "REPO_VERSION:\t${REPO_VERSION}\n" >> ${BUILDINFO}
 touch ${BASHRC}
 printf "export INTROOT=/${BASEDIR}/introot\n" >> ${BASHRC}
 printf "export ACS_CDB=/${BASEDIR}/cdb\n" >> ${BASHRC}
-printf "export TARGETSYS=${TARGETSYS}\n" >> ${BASHRC}
+printf "export STATION=${STATION}\n" >> ${BASHRC}
 printf "export REPO_VERSION=${REPO_VERSION}\n" >> ${BASHRC}
-tar czvf discos-${TARGETSYS}-${BASENAME}-b${BUILD_NUMBER}.tar.gz discos
+tar czvf discos-${STATION}-${BASENAME}-b${BUILD_NUMBER}.tar.gz discos
