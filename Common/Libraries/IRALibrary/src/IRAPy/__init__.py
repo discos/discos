34a1538627872e91353bf48f5ba8ae79eb539c1c
aorlati <a.orlati@ira.inaf.it>
2018-06-14 20:35:43 +0200

    fix #213: buggy python ACS impletation forsed some workaround. Also fixed problem related to the logged error messages to be empty

--- Common/Libraries/IRALibrary/src/IRAPy/__init__.py
+++ Common/Libraries/IRALibrary/src/IRAPy/__init__.py
@@ -9,6 +9,17 @@ list of modules:
 
 """
 import customlogging
-logger = customlogging.getLogger("IRA_CUSTOM_LOGGER")
+import ACSLog
+
+#Some comments required here. The custom logger mechanism is not working in python. 
+#do the way to separate the system logs to the ones do be shown to the user is to use different
+#logging channels.   
+logger = customlogging.getLogger("IRA_SERVICE_LOGGER")
+userLogger = customlogging.getLogger("IRA_CUSTOM_LOGGER")
+userLogger.isUserLogger=True
+
+def _add_user_message(ex,message):
+	ex.addData("Message-to-User",message)
+
 customlogging.Log.setCapacity(1)
 customlogging.Log.setBatchSize(1)
