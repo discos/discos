09e7ab3308e339d55581ee8900e30a99f4015648
Giuseppe Carboni <giuseppecarboni89@live.com>
2019-06-14 10:34:29 +0000

    Splitted WeatherStation::getData in two different methods. See below.
    The two methods are `getData`, which returns the last read weather parameters immediately, and `updateData`, which performs the request to the devIOs tu update every weather parameter.
    The `updateData` method is now called periodically (every 2 seconds) from the `UpdatingThread` (the former `WindCheckerThread`). This thread also checks if the wind is too strong and automatically stows the antenna.

--- Common/Servers/WeatherStation/include/WeatherStationImpl.h
+++ Common/Servers/WeatherStation/include/WeatherStationImpl.h
@@ -33,7 +33,6 @@
 #include <cstdlib>
 #include "WeatherSocket.h"
 #include "WeatherStationData.h"
-// #include "WindCheckerThread.h"
 #include <acsThread.h>
 #include <AntennaBossC.h>
 #include <SchedulerC.h>
@@ -48,7 +47,6 @@
 /*
 const CString ADDRESS="192.167.8.13"; //DEBUG
 const int THREADSLEEPSECONDS=10000; // 1 s in unit of 100ns, from thread sleep time*/
-
 
 using namespace maci; 
 using namespace std; 
@@ -91,162 +89,140 @@ namespace Antenna
 using ACS::ThreadBase;
 class WeatherStationImpl;
 
-class CWindCheckerThread : public ACS::Thread
-{
-
- public:
-	 CWindCheckerThread (const ACE_CString& name,
-			WeatherStationImpl*   weatherStation,
-			const ACS::TimeInterval& responseTime=ThreadBase::defaultResponseTime,
-			const ACS::TimeInterval& sleepTime=ThreadBase::defaultSleepTime) ;
-
-	 ~CWindCheckerThread() { ACS_TRACE("CWindCheckerThread::~CWindCheckerThread"); }
-	 virtual void onStop();
-	 virtual void onStart();
-	 virtual void runLoop();
-
- private:
-		int loopCounter_m;
-                int m_threshold; // wind threshold in km/h
-		WeatherStationImpl  * m_srtweatherstation_p;
-
-};
-
-
-
-
-class  WeatherStationImpl:     public virtual CharacteristicComponentImpl,     //Standard component superclass
-			 public virtual POA_Weather::GenericWeatherStation //CORBA servant stub
-				   
-
+class CUpdatingThread : public ACS::Thread
 {
 public:
-	/**
-	Constructor 
-	
-	* @param CompName component's name. This is also the name that will be used to find the configuration data for the component in the Configuration Database.
-	* @param containerServices p/ACS_SRT/Common/Interfaces/MetrologyInterface/idlointer to the class that exposes all services offered by container
-	
-	
-	*/
-	WeatherStationImpl(const ACE_CString &CompName,
-			maci::ContainerServices *containerServices);
+    CUpdatingThread (const ACE_CString& name,
+                     WeatherStationImpl* weatherStation,
+                     const ACS::TimeInterval& responseTime=ThreadBase::defaultResponseTime,
+                     const ACS::TimeInterval& sleepTime=ThreadBase::defaultSleepTime);
 
+    ~CUpdatingThread() { ACS_TRACE("CUpdatingThread::~CUpdatingThread"); }
+    virtual void onStop();
+    virtual void onStart();
+    virtual void runLoop();
 
-	/**
-	 * Destructor.
-	*/
-	virtual ~WeatherStationImpl(); 
-
-	/**
-	 * This method allows the client to interface the component by sending text commands. The command is parsed and executed according the
-	 * defined command syntax and grammar. This method is required to implement the <i>Managment::CommandInterpreter</i> interface.
-        * @throw CORBA::SystemException
-	 * @throw ManagementErrors::CommandLineErrorEx 
-	 * @param configCommand this string contains the string that will be executed
-	 * @return  answer  the string that reports the command execution results or in case, errors
-	 */
-        virtual CORBA::Boolean  command(const char *cmd,CORBA::String_out answer)   throw (CORBA::SystemException);
-
-   
-	/**
-	*return  the all the data parameters formatted into a 
-	*meteoparameters structure
-	* @return  meteoparameters
-	*/
-	virtual Weather::parameters getData() throw (ACSErr::ACSbaseEx,CORBA::SystemException);
-
-	virtual double getWindSpeedAverage() throw (ACSErr::ACSbaseEx,CORBA::SystemException);
-	virtual double getWindspeedPeak() throw (ACSErr::ACSbaseEx,CORBA::SystemException);
-	virtual double getWindDir() throw (ACSErr::ACSbaseEx,CORBA::SystemException);
-	virtual double getHumidity() throw (ACSErr::ACSbaseEx,CORBA::SystemException);
-	virtual double getTemperature() throw (ACSErr::ACSbaseEx,CORBA::SystemException);
-	virtual double getPressure() throw (ACSErr::ACSbaseEx,CORBA::SystemException);
-
-	virtual void initialize() throw (ACSErr::ACSbaseExImpl);
-	virtual void execute() throw (ACSErr::ACSbaseExImpl);
-	virtual void cleanUp()throw (ACSErr::ACSbaseExImpl);
-	virtual void aboutToAbort() throw (ACSErr::ACSbaseExImpl);
-	
-   void parkAntenna(void);
-
-
-	/** 
-	 * Returns a reference to the air temperature  property Implementation of IDL interface.
-	 * @return pointer to read-write double temperature
-	*/
-	 virtual ACS::RWdouble_ptr  temperature ()
-	throw (CORBA::SystemException);
-
-	 /** 
-	 * Returns a reference to the wind direction  property Implementation of IDL interface.
-	 * @return pointer to read-write double wind direction
-	*/
- 	 virtual ACS::RWdouble_ptr  winddir ()
-	throw (CORBA::SystemException);
-	 /** 
-	 * Returns a reference to the wind speed  property Implementation of IDL interface.
-	 * @return pointer to read-write double wind speed
-	*/
-
- 	 virtual ACS::RWdouble_ptr  windspeed ()
-	throw (CORBA::SystemException);
-	 /** 
-	 * Returns a reference to the wind speed  property Implementation of IDL interface.
-	 * @return pointer to read-write double wind speed
-	*/
-
- 	 virtual ACS::RWdouble_ptr  windspeedpeak ()
-	throw (CORBA::SystemException);
-	 /** 
-	 * Returns a reference to the relative humidity  property Implementation of IDL interface.
-	 * @return pointer to read-write double wind speed
-	*/
-
-  	 virtual ACS::RWdouble_ptr  humidity ()
-	throw (CORBA::SystemException);
-	 /** 
-	 * Returns a reference to the wind speed  property Implementation of IDL interface.
-	 * @return pointer to read-write double relative humidity
-	*/
-  	 virtual ACS::RWdouble_ptr  pressure ()
-	throw (CORBA::SystemException);
-
- 	virtual ACS::ROdouble_ptr autoparkThreshold() 
-	throw (CORBA::SystemException);
-
-
-	double getWind();
-   unsigned int m_threshold;
- 
 private:
-	maci::ContainerServices * m_containerServices;
- 	Management::Scheduler_proxy m_scheduler;
-	Antenna::AntennaBoss_proxy m_antennaBoss;
-	Antenna::Mount_proxy m_mount; 
-	//Antenna::SRTMount_proxy m_mount;        	
-	void deleteAll();
-	CSecureArea<WeatherSocket> *m_socket;
-	CWindCheckerThread *m_controlThread_p;
-	WeatherStationData m_wsdata;
-	CError err;
-   CString ADDRESS;
-	unsigned int PORT;
-	SmartPropertyPointer<RWdouble> m_temperature;
-	SmartPropertyPointer<RWdouble> m_winddir;
-	SmartPropertyPointer<RWdouble> m_windspeed;
-	SmartPropertyPointer<RWdouble> m_windspeedpeak;
-	SmartPropertyPointer<RWdouble> m_humidity;
-	SmartPropertyPointer<RWdouble> m_pressure;
-	SmartPropertyPointer<ROdouble> m_autoParkThreshold;
-	SimpleParser::CParser<WeatherSocket> * m_parser;
-        unsigned int m_elevationStatus;
-        Antenna::ROTCommonModes_ptr m_property;
-
-        
-    void operator=(const WeatherStationImpl&);
-		
+    int loopCounter_m;
+    int m_threshold; // wind threshold in km/h
+    WeatherStationImpl* m_weatherstation_p;
 };
 
+
+class  WeatherStationImpl : public virtual CharacteristicComponentImpl,         //Standard component superclass
+			                public virtual POA_Weather::GenericWeatherStation   //CORBA servant stub
+{
+public:
+    /**
+    Constructor
+    * @param CompName component's name. This is also the name that will be used to find the configuration data for the component in the Configuration Database.
+    * @param containerServices p/ACS_SRT/Common/Interfaces/MetrologyInterface/idlointer to the class that exposes all services offered by container
+    */
+    WeatherStationImpl(const ACE_CString &CompName, maci::ContainerServices *containerServices);
+
+    /**
+    * Destructor.
+    */
+    virtual ~WeatherStationImpl(); 
+
+    /**
+    * This method allows the client to interface the component by sending text commands. The command is parsed and executed according the
+    * defined command syntax and grammar. This method is required to implement the <i>Managment::CommandInterpreter</i> interface.
+    * @throw CORBA::SystemException
+    * @throw ManagementErrors::CommandLineErrorEx 
+    * @param configCommand this string contains the string that will be executed
+    * @return  answer  the string that reports the command execution results or in case, errors
+    */
+    virtual CORBA::Boolean command(const char *cmd, CORBA::String_out answer) throw (CORBA::SystemException);
+
+    /**
+    *return  the all the data parameters formatted into a meteoparameters structure
+    * @return  meteoparameters
+    */
+    virtual Weather::parameters getData();
+    virtual void updateData() throw (ACSErr::ACSbaseEx,CORBA::SystemException);
+
+    virtual double getWindSpeedAverage() throw (ACSErr::ACSbaseEx,CORBA::SystemException);
+    virtual double getWindspeedPeak() throw (ACSErr::ACSbaseEx,CORBA::SystemException);
+    virtual double getWindDir() throw (ACSErr::ACSbaseEx,CORBA::SystemException);
+    virtual double getHumidity() throw (ACSErr::ACSbaseEx,CORBA::SystemException);
+    virtual double getTemperature() throw (ACSErr::ACSbaseEx,CORBA::SystemException);
+    virtual double getPressure() throw (ACSErr::ACSbaseEx,CORBA::SystemException);
+
+    virtual void initialize() throw (ACSErr::ACSbaseExImpl);
+    virtual void execute() throw (ACSErr::ACSbaseExImpl);
+    virtual void cleanUp()throw (ACSErr::ACSbaseExImpl);
+    virtual void aboutToAbort() throw (ACSErr::ACSbaseExImpl);
+
+    void parkAntenna(void);
+
+    /**
+    * Returns a reference to the air temperature  property Implementation of IDL interface.
+    * @return pointer to read-write double temperature
+    */
+    virtual ACS::RWdouble_ptr temperature() throw (CORBA::SystemException);
+
+    /**
+    * Returns a reference to the wind direction property Implementation of IDL interface.
+    * @return pointer to read-write double wind direction
+    */
+    virtual ACS::RWdouble_ptr winddir() throw (CORBA::SystemException);
+
+    /**
+    * Returns a reference to the wind speed property Implementation of IDL interface.
+    * @return pointer to read-write double wind speed
+    */
+    virtual ACS::RWdouble_ptr windspeed() throw (CORBA::SystemException);
+
+    /**
+    * Returns a reference to the wind speed peak property Implementation of IDL interface.
+    * @return pointer to read-write double wind speed peak
+    */
+    virtual ACS::RWdouble_ptr windspeedpeak() throw (CORBA::SystemException);
+
+    /**
+    * Returns a reference to the relative humidity property Implementation of IDL interface.
+    * @return pointer to read-write double humidity
+    */
+    virtual ACS::RWdouble_ptr humidity() throw (CORBA::SystemException);
+
+    /**
+    * Returns a reference to the pressure property Implementation of IDL interface.
+    * @return pointer to read-write double pressure
+    */
+    virtual ACS::RWdouble_ptr pressure() throw (CORBA::SystemException);
+
+    virtual ACS::ROdouble_ptr autoparkThreshold() throw (CORBA::SystemException);
+
+    double getWind();
+    unsigned int m_threshold;
+ 
+private:
+    maci::ContainerServices * m_containerServices;
+    Management::Scheduler_proxy m_scheduler;
+    Antenna::AntennaBoss_proxy m_antennaBoss;
+    Antenna::Mount_proxy m_mount; 
+    //Antenna::SRTMount_proxy m_mount;        	
+    void deleteAll();
+    CSecureArea<WeatherSocket> *m_socket;
+    CUpdatingThread *m_controlThread_p;
+    WeatherStationData m_wsdata;
+    CError err;
+    CString ADDRESS;
+    unsigned int PORT;
+    SmartPropertyPointer<RWdouble> m_temperature;
+    SmartPropertyPointer<RWdouble> m_winddir;
+    SmartPropertyPointer<RWdouble> m_windspeed;
+    SmartPropertyPointer<RWdouble> m_windspeedpeak;
+    SmartPropertyPointer<RWdouble> m_humidity;
+    SmartPropertyPointer<RWdouble> m_pressure;
+    SmartPropertyPointer<ROdouble> m_autoParkThreshold;
+    SimpleParser::CParser<WeatherSocket> * m_parser;
+    unsigned int m_elevationStatus;
+    Antenna::ROTCommonModes_ptr m_property;
+    void operator=(const WeatherStationImpl&);
+    Weather::parameters m_parameters;
+    BACIMutex m_meteoParametersMutex;
+};
 
 #endif
