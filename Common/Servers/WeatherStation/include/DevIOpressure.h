5b5b7532a43f16175bba7d34e0dfa47b56a1f37f
Giuseppe Carboni <giuseppecarboni89@live.com>
2019-06-20 08:28:49 +0000

    Fix #413, reworker weather station devIOs behavior

--- Common/Servers/WeatherStation/include/DevIOpressure.h
+++ Common/Servers/WeatherStation/include/DevIOpressure.h
@@ -10,16 +10,13 @@
 /* Who                                when            What                                              */
 /* Sergio  Poppi(spoppi@oa-cagliari.inaf.it)  	02/01/2009     Creation                                         */
 
-
+#include <baci.h>
 #include <baciDevIO.h>
 #include <IRA>
-#include <map>
-#include "WeatherSocket.h"
-#include "WeatherStationData.h"
 using namespace IRA;
 
 /**
- * This class is derived from template DevIO and it is used by the temperature  property  of the MeteoStation component
+ * This class is derived from template DevIO and it is used by the pressure property  of the MeteoStation component
  * @author <a href=mailto:spoppi@oa-cagliari.inaf.it>Sergio Poppi</a>,
  * Istituto di Radioastronomia, Italia<br> 
 */
@@ -29,11 +26,9 @@ public:
 	
  	/** 
 	 * Constructor
-	 * @param Socket pointer to a SecureArea that proctects a the  socket. This object must be already initialized and configured.
 	*/
-	DevIOPressure(CSecureArea<WeatherSocket>* socket):m_socket(socket)
+	DevIOPressure()
 	{		
- 		m_initparser=false;
 		AUTO_TRACE("DevIOPressure::DevIOPressure()");		
 	}
 
@@ -56,43 +51,29 @@ public:
 	
 	/**
 	 * Used to read the property value.
-	 * @throw ComponentErrors::PropertyError
 	 * @param timestamp epoch when the operation completes
 	*/ 
-	CORBA::Double  read(ACS::Time& timestamp) throw (ACSErr::ACSbaseExImpl)
+	CORBA::Double read(ACS::Time& timestamp)
 	{
-		// get the CommandLine .......
-		try {
-			CError err;
-			CString rdata="";
-			CSecAreaResourceWrapper<WeatherSocket> sock=m_socket->Get();
-  			m_val=sock->getPressure();
-		}
-		catch (ACSErr::ACSbaseExImpl& E) {
-			_ADD_BACKTRACE(ComponentErrors::PropertyErrorExImpl,dummy,E,"DevIOPressure::read()");
-			dummy.setPropertyName("pressure");
-			dummy.setReason("Property could not be read");
-			//_IRA_LOGGUARD_LOG_EXCEPTION(m_logGuard,dummy,LM_DEBUG);
-			throw dummy;
-		} 				
-		timestamp=getTimeStamp();  //complition time
-		return m_val;
+        timestamp = getTimeStamp();
+        baci::ThreadSyncGuard guard(&m_mutex);
+        return m_val;
 	}
 	/**
-	 * It writes values into controller. Unused because the properties are read-only.
+	 * It writes values into controller.
+     * @param value the new value to set
+	 * @param timestamp epoch when the operation completes
 	*/ 
-	void write(const CORBA::Double& value, ACS::Time& timestamp) throw (ACSErr::ACSbaseExImpl)
+	void write(const CORBA::Double& value, ACS::Time& timestamp)
 	{
-		timestamp=getTimeStamp();
-		return;
+		timestamp = getTimeStamp();
+        baci::ThreadSyncGuard guard(&m_mutex);
+        m_val = value;
 	}
 	
 private:
-	CSecureArea<WeatherSocket>* m_socket;
 	CORBA::Double m_val;
-	WeatherStationData m_wsdata; 
-
-	bool m_initparser;
+    BACIMutex m_mutex;
  };
 
 
