4c1a116c4d07fd7d6d810bc9a1c9060315a560b5
TheDebbio <debbiojump@gmail.com>
2020-11-26 17:24:33 +0100

    env test fix 3

--- Medicina/Servers/MedicinaCBand/src/ComponentCore.cpp
+++ Medicina/Servers/MedicinaCBand/src/ComponentCore.cpp
@@ -1072,7 +1072,7 @@ void CComponentCore::updateEnvironmentTemperature() throw (ReceiversErrors::Rece
 {
     // not under the mutex protection because the m_control object is thread safe (at the micro controller board stage)
     try {
-        m_environmentTemperature.temperature = m_control->vertexTemperature(Helpers::voltage2Celsius);
+        m_environmentTemperature.temperature = m_control->vertexTemperature(Helpers::voltageConverter);
         m_environmentTemperature.timestamp = getTimeStamp();
     }
     catch (IRA::ReceiverControlEx& ex) {
@@ -1089,8 +1089,7 @@ void CComponentCore::updateShieldTemperature() throw (ReceiversErrors::ReceiverC
 {
     // not under the mutex protection because the m_control object is thread safe (at the micro controller board stage)
     try {
-        //m_shieldTemperature.temperature = m_control->cryoTemperature(0,Helpers::voltage2Kelvin);
-        m_shieldTemperature.temperature = m_control->cryoTemperature(0,nullptr);
+        m_shieldTemperature.temperature = m_control->cryoTemperature(0,Helpers::voltage2Kelvin);        
         m_shieldTemperature.timestamp = getTimeStamp();
     }
     catch (IRA::ReceiverControlEx& ex) {
