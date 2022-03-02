08ac4a9bd60549ba5b5504204ac7712d8e5540ab
Giuseppe Carboni <giuseppecarboni89@live.com>
2022-03-02 12:46:16 +0100

    Fix #684, fixed tests for the SRTMinorServoCommandLibrary and its python wrapper (#685)

---
+++ SRT/Libraries/SRTMinorServoLibrary/tests/unittest.cpp
@@ -0,0 +1,53 @@
+#include "gtest/gtest.h"
+#include <IRA>
+#include "SRTMinorServoCommandLibrary.h"
+
+using namespace IRA;
+
+TEST(SRTMinorServoCommandLibraryTest, status)
+{
+    EXPECT_EQ(SRTMinorServoCommandLibrary::status(), "STATUS\r\n");
+    EXPECT_EQ(SRTMinorServoCommandLibrary::status("PFP"), "STATUS=PFP\r\n");
+}
+
+TEST(SRTMinorServoCommandLibraryTest, setup)
+{
+    EXPECT_EQ(SRTMinorServoCommandLibrary::setup("CCB"), "SETUP=CCB\r\n");
+}
+
+TEST(SRTMinorServoCommandLibraryTest, stow)
+{
+    EXPECT_EQ(SRTMinorServoCommandLibrary::stow("PFP"), "STOW=PFP,1\r\n");
+    EXPECT_EQ(SRTMinorServoCommandLibrary::stow("PFP", 2), "STOW=PFP,2\r\n");
+}
+
+TEST(SRTMinorServoCommandLibraryTest, stop)
+{
+    EXPECT_EQ(SRTMinorServoCommandLibrary::stop("PFP"), "STOP=PFP\r\n");
+}
+
+TEST(SRTMinorServoCommandLibraryTest, preset)
+{
+    EXPECT_EQ(SRTMinorServoCommandLibrary::preset("PFP", std::vector<double>{0.,1.,2.,3.,4.,5.}), "PRESET=PFP,0,1,2,3,4,5\r\n");
+}
+
+TEST(SRTMinorServoCommandLibraryTest, programTrack)
+{
+    double start_time = CIRATools::getUNIXEpoch() + 3;
+    unsigned long int trajectory_id = (unsigned long int)(start_time * 1000);
+    std::stringstream expected_answer;
+    expected_answer << "PROGRAMTRACK=PFP," << trajectory_id << ",0," << trajectory_id << ",0,1,2,3,4,5\r\n";
+    EXPECT_EQ(SRTMinorServoCommandLibrary::programTrack("PFP", trajectory_id, 0, std::vector<double>{0.,1.,2.,3.,4.,5.}, start_time), expected_answer.str());
+
+    for(unsigned int i = 1; i < 10; i++)
+    {
+        expected_answer.str(std::string());
+        expected_answer << "PROGRAMTRACK=PFP," << trajectory_id << "," << i << ",*,0,1,2,3,4,5\r\n";
+        EXPECT_EQ(SRTMinorServoCommandLibrary::programTrack("PFP", trajectory_id, i, std::vector<double>{0.,1.,2.,3.,4.,5.}), expected_answer.str());
+    }
+}
+
+TEST(SRTMinorServoCommandLibraryTest, offset)
+{
+    EXPECT_EQ(SRTMinorServoCommandLibrary::offset("PFP", std::vector<double>{0.,1.,2.,3.,4.,5.}), "OFFSET=PFP,0,1,2,3,4,5\r\n");
+}
