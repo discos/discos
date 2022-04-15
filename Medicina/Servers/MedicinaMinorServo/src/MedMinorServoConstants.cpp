071efcdbde00f3728102bc598c0724164f54bead
Fabio Vitello <fvitello@users.noreply.github.com>
2022-04-15 08:20:25 +0200

    Removed old struct initialization

--- Medicina/Servers/MedicinaMinorServo/src/MedMinorServoConstants.cpp
+++ Medicina/Servers/MedicinaMinorServo/src/MedMinorServoConstants.cpp
@@ -11,13 +11,4 @@ MedMinorServoConstants* MedMinorServoConstants::getInstance() {
 
  
 MedMinorServoConstants::MedMinorServoConstants(){
-
-    MINOR_SERVO_X = { "X", 0, 0, 0.0, 0.05, 0.01, 14.0 };
-    MINOR_SERVO_YP = { "YP", 0, 0, 0.0, 0.05, 0.01, 8.0 };
-    MINOR_SERVO_Y = { "Y", 0, 0, 0.0, 0.05, 0.01, 14.0 };
-    MINOR_SERVO_ZP = { "ZP", 0, 0 , 0.0, 0.05, 0.01, 8.0 };
-    MINOR_SERVO_Z1 = { "Z1", 0, 0, 0.0, 0.05, 0.01, 14.0 };
-    MINOR_SERVO_Z2 = { "Z2", 0, 0, 0.0, 0.05, 0.01, 14.0 };
-    MINOR_SERVO_Z3 = { "Z3", 0, 0, 0.0, 0.05, 0.01, 14.0 };
-
 }
